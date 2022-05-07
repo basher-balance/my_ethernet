dc018952eb3eca83dec33b5d56e2f67915bf2551
MitiSit <mitisit89@gmail.com>
2022-05-07 16:35:58 +0300

    litned by prettier

--- pyss/static/js/index.js
+++ pyss/static/js/index.js
@@ -1,62 +1,61 @@
-  const container = document.querySelector('#twitch')
-  const TOKEN="YOUR_TOKEN"
-    async function deleteChannel(channelId) {
-      try {
-        const res = await fetch('/twitch/channels', {
-          method: 'DELETE',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ id: channelId })
-        })
+const container = document.querySelector('#twitch')
+const TOKEN = 'YOUR_TOKEN'
+async function deleteChannel (channelId) {
+  try {
+    const res = await fetch('/twitch/channels', {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id: channelId })
+    })
 
-        return await res.json()
-      } catch (err) {
-        console.error(err.message)
-      }
-    }
+    return await res.json()
+  } catch (err) {
+    console.error(err.message)
+  }
+}
 
-    async function main() {
-      const res = await fetch('/twitch/channels', {
-        method: 'GET',
-        hedears:{ "X-CSRFToken": getCookie("csrftoken")}
-      })
-      const channels = await res.json()
+async function main () {
+  const res = await fetch('/twitch/channels', {
+    method: 'GET',
+    hedears: { 'X-CSRFToken': getCookie('csrftoken') }
+  })
+  const channels = await res.json()
 
-      for (const channel of channels.streamers) {
-        const player = document.createElement('div')
-        player.classList.add('column')
+  for (const channel of channels.streamers) {
+    const player = document.createElement('div')
+    player.classList.add('column')
 
-        const button = document.createElement('button')
-        button.classList.add('button', 'is-danger', 'is-outlined')
+    const button = document.createElement('button')
+    button.classList.add('button', 'is-danger', 'is-outlined')
 
-        const span = document.createElement('span')
-        span.classList.add('icon', 'is-small')
+    const span = document.createElement('span')
+    span.classList.add('icon', 'is-small')
 
-        const icon = document.createElement('i')
-        icon.classList.add('fas', 'fa-times')
+    const icon = document.createElement('i')
+    icon.classList.add('fas', 'fa-times')
 
-        button.addEventListener('click', () => {
-          const res = deleteChannel(channel.id)
-          embed.pause()
-          embed.disableCaptions()
-          player.remove()
-        })
+    button.addEventListener('click', () => {
+      const res = deleteChannel(channel.id)
+      embed.pause()
+      embed.disableCaptions()
+      player.remove()
+    })
 
-        const embed = new Twitch.Embed(player, {
-          theme: 'dark',
-          autoplay: false,
-          muted: true,
-          width: 854,
-          height: 480,
-          channel: channel.streamer
-        })
+    const embed = new Twitch.Embed(player, {
+      theme: 'dark',
+      autoplay: false,
+      muted: true,
+      width: 854,
+      height: 480,
+      channel: channel.streamer
+    })
 
-        player.appendChild(embed._iframe)
-        span.appendChild(icon)
-        button.appendChild(span)
-        player.appendChild(button)
-        container.appendChild(player)
-      }
-    }
+    player.appendChild(embed._iframe)
+    span.appendChild(icon)
+    button.appendChild(span)
+    player.appendChild(button)
+    container.appendChild(player)
+  }
+}
 
-    main()
-
+main()
