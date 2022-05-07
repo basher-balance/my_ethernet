cccab5b57737bb343099a3f52bcde9c8fbffa8e8
MitiSit <mitisit89@gmail.com>
2022-05-07 16:49:27 +0300

    fix

--- pyss/static/js/index.js
+++ pyss/static/js/index.js
@@ -1,11 +1,27 @@
 const container = document.querySelector('#twitch')
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
 async function deleteChannel (channelId) {
   try {
     const res = await fetch('/twitch/channels', {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
-        'X-CSRFToken': getCookie('csrftoken')
+        'X-CSRFToken': csrftoken,
       },
       body: JSON.stringify({ id: channelId })
     })
