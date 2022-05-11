507418f42f5cd0e43a527ace0944860010812c3d
MitiSit <mitisit89@gmail.com>
2022-05-11 18:05:56 +0300

    rework view

--- weathers/views.py
+++ weathers/views.py
@@ -1,18 +1,22 @@
+from django.http import JsonResponse
+from .keys import appid
 from django.shortcuts import render, redirect
-from .models import Weather, Weather_Today
+import requests
+
+
+url = "https://api.openweathermap.org/data/2.5/onecall"
+
+params = dict(
+    lat=54.87, lon=69.16, exlude="dayliy", lang="ru", units="metric", appid=appid
+)
 
 
 def weather(request):
-    """Выводит данные из моделей Weather и Weather_Today"""
-    weather_week = list(Weather.objects.all().values_list("weather_daily"))
-    weather_today = list(Weather_Today.objects.values())
-    # weather_diagram = list(Weather.objects.all().values_list('diagram'))
-    return render(
-        request,
-        "weathers/weather.html",
-        {
-            "weather_week": weather_week[0][0][1:],
-            "weather_today": weather_today[0],
-            #'weather_diagram': weather_diagram[0][0][1:],
-        },
-    )
+    return render(request, "weathers/weathers.html")
+
+
+def open_weather_api(req):
+    if req.method == "GET":
+        payload = requests.get(url=url, params=params)
+        data = payload.json()
+        return JsonResponse(data)
