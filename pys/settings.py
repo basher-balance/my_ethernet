0d9dbfca7739f28732b8d61382040a6a72aa96cb
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-05-11 21:13:03 +0800

    feat: added links to database models to header

--- pys/settings.py
+++ pys/settings.py
@@ -90,6 +90,7 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "pyss.context.model_paths",
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
