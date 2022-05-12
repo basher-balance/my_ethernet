6e3f52b7379e00b8ae0a26f04a757a9eceb6c7f9
MitiSit <mitisit89@gmail.com>
2022-05-12 14:52:21 +0300

    apply changes from @crashmax-dev

--- weathers/views.py
+++ weathers/views.py
@@ -2,21 +2,29 @@ from django.http import JsonResponse
 from .keys import appid
 from django.shortcuts import render, redirect
 import requests
-
-
-url = "https://api.openweathermap.org/data/2.5/onecall"
-
-params = dict(
-    lat=54.87, lon=69.16, exlude="dayliy", lang="ru", units="metric", appid=appid
-)
+import json
 
 
 def weather(request):
     return render(request, "weathers/weather.html")
 
 
-def open_weather_api(req):
-    if req.method == "GET":
+def weather_api(request):
+    if request.method == "POST":
+        body = json.loads(request.body)
+        lat = body.get("lat", 54.87)
+        lon = body.get("lon", 69.16)
+
+        url = "https://api.openweathermap.org/data/2.5/onecall"
+        params = dict(
+            lat=lat,
+            lon=lon,
+            # exclude="daily",
+            lang="ru",
+            units="metric",
+            appid=appid,
+        )
+
         payload = requests.get(url=url, params=params)
         data = payload.json()
         return JsonResponse(data)
