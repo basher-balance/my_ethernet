d2ed3d3c2aea23bffd0774e804188ab3de7f9d4c
MitiSit <mitisit89@gmail.com>
2022-05-12 16:48:28 +0300

    rework waethers view

--- weathers/views.py
+++ weathers/views.py
@@ -1,30 +1,45 @@
 from django.http import JsonResponse
 from .keys import appid
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 import requests
 import json
+from django.core.exceptions import BadRequest
 
 
 def weather(request):
     return render(request, "weathers/weather.html")
 
-
-def weather_api(request):
+#TODO refactoring 
+def weather_direct_api(request):
     if request.method == "POST":
         body = json.loads(request.body)
-        lat = body.get("lat", 54.87)
-        lon = body.get("lon", 69.16)
+        query = body.get("query")
+
+        url = "https://api.openweathermap.org/geo/1.0/direct"
+        options = dict(q=query)
+        response = weather_request(url, options)
+        if not len(response):
+            raise BadRequest
+        return JsonResponse(response, safe=False)
+
+
+def weather_onecall_api(request):
+    if request.method == "POST":
+        body = json.loads(request.body)
 
         url = "https://api.openweathermap.org/data/2.5/onecall"
-        params = dict(
-            lat=lat,
-            lon=lon,
-            # exclude="daily",
-            lang="ru",
-            units="metric",
-            appid=appid,
-        )
+        options = dict(lat=body.get("lat", 54.87), lon=body.get("lon", 69.16))
 
-        payload = requests.get(url=url, params=params)
-        data = payload.json()
-        return JsonResponse(data)
+        response = weather_request(url, options)
+        return JsonResponse(response)
+
+
+def weather_request(url, options):
+    params = dict(limit=1, lang="ru", units="metric", appid=appid)
+
+    params.update(options)
+
+    response = requests.get(url=url, params=params)
+
+    data = response.json()
+    return data
