1ed47ecc1097c2b2290b024ecc2328b3c63f5f74
MitiSit <mitisit89@gmail.com>
2022-05-17 19:37:26 +0300

    Revert "fix"
    This reverts commit 45d4ebca30189d5aef44022c93ca32ff9af94bd5, reversing
    changes made to 2a10495ed17edf41eeba011ef501c580afc94b2e.

--- task_scheduler/vk_periodic_tasks.py
+++ task_scheduler/vk_periodic_tasks.py
@@ -34,14 +34,7 @@ def posts_vk_group():
     sorted_all_respons = sorted(all_respons, reverse=True, key=itemgetter("date"))
     for sorted_all_respon in sorted_all_respons:
         try:
-<<<<<<< HEAD
             link_post = fr"http://{sorted_all_respon['text'].split(': http://')[1].split('/n')[0]}"
-=======
-             link_post = (
-                "http://"
-                + sorted_all_respon["text"].split(": http://")[1].split("\n")[0]
-            )
->>>>>>> main
         except IndexError:
             link_post = "http://habr.com"
         try:
