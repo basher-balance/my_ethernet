dd186528f362a3589f41708e02b1b26e0830ac15
MitiSit <mitisit89@gmail.com>
2022-05-17 03:58:05 +0300

    refactor weather view

--- weathers/urls.py
+++ weathers/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "weathers"
 urlpatterns = [
     # страница погоды
-    path("weather", views.weather, name="weather"),
+    path("weather", views.WeatherView.as_view(), name="weather"),
     path("weather/forecast", views.weather_onecall_api),
     path("weather/search", views.weather_direct_api),
 ]
