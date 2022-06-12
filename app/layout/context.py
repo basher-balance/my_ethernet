a7ab955e83afbc9dd9601a9bed34b9892a574dd2
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-06-12 22:17:27 +0800

    breaking changes commit!

---
+++ app/layout/context.py
@@ -0,0 +1,12 @@
+import django.apps
+
+
+def model_paths(request):
+    model_paths = []
+    models = django.apps.apps.get_models()[:-7]
+
+    for model in models:
+        path = str(model._meta).split(".")
+        model_paths.append("/".join(path))
+
+    return {"model_paths": model_paths}
---
+++ app/task_scheduler/twitch_periodic_tasks.py
@@ -0,0 +1,69 @@
+import logging
+import pickle
+import requests
+import os
+
+from .tasks import process_user_stats
+from twitch.models import Twitch_model
+from twitchAPI.twitch import Twitch
+from twitchAPI.oauth import UserAuthenticator
+from twitchAPI.types import AuthScope
+from .managed_file import ManagedFile
+from core.settings import BASE_DIR
+
+
+def twitch_parse():
+    logging.warning("It is time to start the dramatiq task twitch")
+    Twitch_model.objects.all().delete()
+    token_tw = f"{BASE_DIR}/layout/token_tw.pickle"
+    refresh_token_tw = f"{BASE_DIR}/layout/refresh_token_tw.pickle"
+    client_id = os.environ.get("TWITCH_CLIENT_ID")
+    client_secret = os.environ.get("TWITCH_CLIENT_SECRET")
+    twitch_channel_id = os.environ.get("TWITCH_CHANNEL_ID")
+
+    body = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "client_credentials",
+    }
+
+    r = requests.post("https://id.twitch.tv/oauth2/token", body)
+
+    # data output
+    keys_data = r.json()
+
+    headers = {
+        "Client-ID": client_id,
+        "Authorization": f'Bearer {keys_data.get("access_token")}',
+    }
+
+    twitch = Twitch(client_id, client_secret)
+    target_scope = [AuthScope.USER_READ_FOLLOWS]
+    auth = UserAuthenticator(twitch, target_scope, force_verify=False)
+
+    try:
+        with (
+            ManagedFile(token_tw, "rb") as t,
+            ManagedFile(refresh_token_tw, "rb") as r_t,
+        ):
+            print("Reading token and refresh_token")
+            token = pickle.load(t)
+            refresh_token = pickle.load(r_t)
+        twitch.set_user_authentication(token, target_scope, refresh_token)
+        online = twitch.get_followed_streams(twitch_channel_id)
+    except:
+        token, refresh_token = auth.authenticate()
+        with (
+            ManagedFile(token_tw, "wb") as new_token,
+            ManagedFile(refresh_token_tw, "wb") as new_ref_token,
+        ):
+            pickle.dump(token, new_token)
+            pickle.dump(refresh_token, new_ref_token)
+        twitch.set_user_authentication(token, target_scope, refresh_token)
+        online = twitch.get_followed_streams(twitch_channel_id)
+    finally:
+        for k in range(len(online["data"])):
+            writting_streamer_in_bd, created = Twitch_model.objects.update_or_create(
+                streamer=online["data"][k]["user_login"]
+            )
+    process_user_stats.send()
